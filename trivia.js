a68d71cd1833efb307aa33c55046bf61feb4abf8
Cameron <cameroncros@gmail.com>
2014-12-26 00:41:26 +1100

    Update trivia.js
    Works now

--- trivia.js
+++ trivia.js
@@ -1,4 +1,7 @@
-var answers = [];
+var answers_solver = [];
+
+var question_solver = "";
+var options_solver = [];
 
 xmlhttp=new XMLHttpRequest();
 xmlhttp.onreadystatechange=function()
@@ -8,10 +11,9 @@ xmlhttp.onreadystatechange=function()
       var lines = xmlhttp.responseText.split("\n");
       for (var i = 0; i < lines.length/3; i++) 
       {
-        answers.push({question:lines[i*3], answer:lines[i*3+1]});
+        answers_solver.push({question:lines[i*3], answer:lines[i*3+1]});
       }
-      alert(answers);
-      debugger;
+      getQuestionsAndOptions();
       solveQuestion();
     }
   }
@@ -19,5 +21,26 @@ xmlhttp.open("GET","https://gist.githubusercontent.com/cameroncros/4e0fe4c3bc188
 xmlhttp.send();
 
 function solveQuestion() {
-
+  for (var i = 0; i < answers_solver.length; i++) {
+    if (answers_solver[i].question == question_solver) {
+      for (var j = 0; j < 4; j++) {
+        if (answers_solver[i].answer == options_solver[j]) {
+          alert(j);
+          trivia_submitAnswer(j);
+          return;
+        }
+      }
+    }
+  }
+  alert("Failed to find answer, prehaps its not in the correct format, or missing entirely from the data set, please report with a screenshot of the question")
 }
+
+function getQuestionsAndOptions() {
+  question_solver = document.getElementById("tomo_question").innerHTML.trim();
+  options_solver.push(document.getElementById("answer0").innerHTML.trim());
+  options_solver.push(document.getElementById("answer1").innerHTML.trim());
+  options_solver.push(document.getElementById("answer2").innerHTML.trim());
+  options_solver.push(document.getElementById("answer3").innerHTML.trim());
+}
+
+
