c8d19976d29c6d04bb6f97dcfef08bbd1f783bc0
Cameron <cameroncros@gmail.com>
2015-02-18 22:49:28 +1100

    Create hoard.js

---
+++ hoard.js
@@ -0,0 +1,9 @@
+var invent = document.getElementById("invent");
+var items = invent.getElementsByTagName("span");
+
+for (var i = 0; i < lines.length/3; i++) {
+  var inputs = items[i].getElementsByTagName("input");
+  inputs[2].value
+}
+
+function isSpecial() {
